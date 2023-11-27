0ca31a173ff1debfae543a970d31a7e8a2b08483
Carmen Caniglia <carmencaniglia@gmail.com>
2023-11-27 14:24:25 +0100

    commit prova

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n1 = 4;
+        int n2 = 6;
+        int result = n1 * n2;
+
+        System.out.println("Il risultato della moltiplicazione è:" + result);
     }
-    
+
 }
